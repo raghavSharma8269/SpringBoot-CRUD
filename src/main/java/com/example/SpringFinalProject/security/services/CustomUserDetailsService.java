29561121a7bdc5ddb4c77223bf56692dd4fb7eea
raghavsharma <r.sharmali2023@gmail.com>
2025-01-20 13:24:34 -0500

    added logger to CustomUserDetailsService

--- src/main/java/com/example/SpringFinalProject/security/services/CustomUserDetailsService.java
+++ src/main/java/com/example/SpringFinalProject/security/services/CustomUserDetailsService.java
@@ -2,6 +2,8 @@ package com.example.SpringFinalProject.security.services;
 
 import com.example.SpringFinalProject.security.CustomUser;
 import com.example.SpringFinalProject.security.CustomUserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -16,6 +18,7 @@ import java.util.List;
 public class CustomUserDetailsService implements UserDetailsService {
     
     private final CustomUserRepository userRepository;
+    private final Logger logger = LoggerFactory.getLogger(CustomUserDetailsService.class);
     
     public CustomUserDetailsService(CustomUserRepository userRepository) {
         this.userRepository = userRepository;
@@ -23,6 +26,9 @@ public class CustomUserDetailsService implements UserDetailsService {
     
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        logger.info("Executing " + getClass() + " input: " + username);
+
         CustomUser user = userRepository.findById(username)
             .orElseThrow(() -> new UsernameNotFoundException("User not found: " + username));
         
